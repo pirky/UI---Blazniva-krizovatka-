7d1a965669e0e7edee1c46d56673d9bcf0cbee8d
Pirky <m.pirkovsky@gmail.com>
2020-10-19 18:11:44 +0200

    Initial commit

---
+++ zadanie_2.py
@@ -0,0 +1,16 @@
+
+
+class Car:
+    def __init__(self, color, size, line, column, position):
+        self.color = color
+        self.size = size
+        self.line = line
+        self.column = column
+        self.position = position
+
+
+def start():
+    print("uičko")
+
+
+start()
