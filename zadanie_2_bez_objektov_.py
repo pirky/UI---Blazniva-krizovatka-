7e13c9e8a29255da32d1463b8917a384ac4d6a8a
Pirky <m.pirkovsky@gmail.com>
2020-10-24 18:04:56 +0200

    opravena implementacia s visited polom

--- zadanie_2_bez_objektov_.py
+++ zadanie_2_bez_objektov_.py
@@ -1,6 +1,14 @@
 import time
+from colorama import Fore
+
 
 cars = {}
+visited = []
+visited_depths = []
+stack = []
+stack_depths = []
+path = []
+path_depths = []
 
 
 def init_bez_objektov():
@@ -22,15 +30,93 @@ def init_bez_objektov():
     return crossroad
 
 
-def print_crossroad_bez_objektov(crossroad):
+def print_operators(crossroad_1, crossroad_2):
+    cars_1 = {}
+    cars_2 = {}
+    car_line = 0
+    for line in crossroad_1:
+        car_column = 0
+        for char in line:
+            if char != "0":
+                if char not in cars_1:
+                    cars_1[char] = {"line": car_line, "column": car_column, "size": 1}
+                else:
+                    cars_1[char]["size"] += 1
+            car_column += 1
+        car_line += 1
+
+    car_line = 0
+    for line in crossroad_2:
+        car_column = 0
+        for char in line:
+            if char != "0":
+                if char not in cars_2:
+                    cars_2[char] = {"line": car_line, "column": car_column, "size": 1}
+                else:
+                    cars_2[char]["size"] += 1
+            car_column += 1
+        car_line += 1
+
+    car_name = 65
+    for i in range(len(cars_1)):
+        if cars_1[chr(car_name + i)] != cars_2[chr(car_name + i)]:
+            if cars_1[chr(car_name + i)]["line"] == cars_2[chr(car_name + i)]["line"]:
+                move = cars_1[chr(car_name + i)]["column"] - cars_2[chr(car_name + i)]["column"]
+                if move < 0:
+                    print("\n\"{}\" -> {} right.".format(chr(car_name + i), abs(move)))
+                else:
+                    print("\n\"{}\" -> {} left.".format(chr(car_name + i), abs(move)))
+            else:
+                move = cars_1[chr(car_name + i)]["line"] - cars_2[chr(car_name + i)]["line"]
+                if move < 0:
+                    print("\n\"{}\" -> {} down.".format(chr(car_name + i), abs(move)))
+                else:
+                    print("\n\"{}\" -> {} up.".format(chr(car_name + i), abs(move)))
+
+
+def print_crossroad(crossroad):
+    print()
     for line in crossroad:
-        print(line)
-    print("\n")
+        for char in line:
+            if char == "0":
+                print(char, end=' ')
+            elif ord(char) % 15 == 0:
+                print(Fore.LIGHTYELLOW_EX + char, end=' ')
+            elif ord(char) % 15 == 1:
+                print(Fore.LIGHTBLACK_EX + char, end=' ')
+            elif ord(char) % 15 == 2:
+                print(Fore.LIGHTWHITE_EX + char, end=' ')
+            elif ord(char) % 15 == 4:
+                print(Fore.LIGHTRED_EX + char, end=' ')
+            elif ord(char) % 15 == 5:
+                print(Fore.RED + char, end=' ')
+            elif ord(char) % 15 == 6:
+                print(Fore.CYAN + char, end=' ')
+            elif ord(char) % 15 == 7:
+                print(Fore.YELLOW + char, end=' ')
+            elif ord(char) % 15 == 8:
+                print(Fore.GREEN + char, end=' ')
+            elif ord(char) % 15 == 9:
+                print(Fore.BLUE + char, end=' ')
+            elif ord(char) % 15 == 10:
+                print(Fore.MAGENTA + char, end=' ')
+            elif ord(char) % 15 == 11:
+                print(Fore.LIGHTBLUE_EX + char, end=' ')
+            elif ord(char) % 15 == 12:
+                print(Fore.LIGHTCYAN_EX + char, end=' ')
+            elif ord(char) % 15 == 13:
+                print(Fore.LIGHTGREEN_EX + char, end=' ')
+            elif ord(char) % 15 == 14:
+                print(Fore.LIGHTMAGENTA_EX + char, end=' ')
+            print(Fore.RESET, end='')
+
+        print()
 
 
 def operator_bez_objektov(crossroad, car_name, direction):
     global cars
     first = car_line = car_column = i = 0
+
     for line in crossroad:
         j = 0
         for char in line:
@@ -52,6 +138,7 @@ def operator_bez_objektov(crossroad, car_name, direction):
                         car_column = j
             j += 1
         i += 1
+
     for i in range(cars[car_name]["size"]):
         if direction == "right" or direction == "left":
             crossroad[car_line][car_column + i] = car_name
@@ -59,7 +146,6 @@ def operator_bez_objektov(crossroad, car_name, direction):
             crossroad[car_line + i][car_column] = car_name
 
 
-# vrati poziciu auta
 def find_car_bez_objektov(crossroad, car_name):
     i = 0
     for line in crossroad:
@@ -77,6 +163,7 @@ def max_step_bez_objektov(crossroad, car_name, direction):
     steps = 0
     car_line, car_column = find_car_bez_objektov(crossroad, car_name)
     car_size = cars[car_name]["size"]
+
     if direction == "right":
         while car_column + car_size + steps < 6:
             field = crossroad[car_line][car_column + car_size + steps]
@@ -108,68 +195,90 @@ def max_step_bez_objektov(crossroad, car_name, direction):
     return steps
 
 
-def move_combo_bez_objektov(crossroad, car_name, visited, visited_depths, stack, stack_depths, curr_depth, depth, direction):
+def move_combo_bez_objektov(crossroad, car_name, curr_depth, depth, direction):
     global cars
+    global visited
+    global visited_depths
+    global stack
+    global stack_depths
+
     steps = max_step_bez_objektov(crossroad, car_name, direction)
-    temp = [list(x) for x in crossroad]
     curr_depth += 1
     if curr_depth > depth:
-        return 0
+        return
     for step in range(1, steps + 1):
-        temp = [list(x) for x in temp]
-        operator_bez_objektov(temp, car_name, direction)
+        crossroad = [list(x) for x in crossroad]
+        operator_bez_objektov(crossroad, car_name, direction)
 
-        if temp in visited:
-            index = visited.index(temp)
+        if crossroad in visited:
+            index = visited.index(crossroad)
             if curr_depth < visited_depths[index]:
-                stack.append(temp)
+                stack.append(crossroad)
                 stack_depths.append(curr_depth)
-                visited[index] = temp
+                visited[index] = crossroad
                 visited_depths[index] = curr_depth
         else:
-            stack.append(temp)
+            stack.append(crossroad)
             stack_depths.append(curr_depth)
 
-        for i in range(len(temp)):     # prvé (červené) auto sa nachádza na konci križovatky
-            if temp[i][5] == 'A':
-                print("finito")
-                return 1
-    return 0
+
+def update_path(curr_depth, crossroad):
+    global path
+    global path_depths
+    if path_depths[-1] < curr_depth:
+        path.append(crossroad)
+        path_depths.append(curr_depth)
+    elif path_depths[-1] == curr_depth:
+        path[-1] = crossroad
+    else:
+        while path_depths[-1] > curr_depth:
+            path.pop()
+            path_depths.pop()
+        path[-1] = crossroad
+        path_depths[-1] = curr_depth
 
 
 def dfs_bez_objektov(initial_crossroad, depth):
     global cars
+    global visited
+    global visited_depths
+    global stack
+    global stack_depths
+    global path
+    global path_depths
     curr_depth = 0
     stack = [initial_crossroad]
     stack_depths = [curr_depth]
+    path = [initial_crossroad]
+    path_depths = [curr_depth]
     visited = []
     visited_depths = []
     while True:
         if len(stack) == 0:
             print("{} depth. Stack empty. Mission failed going deeper.".format(depth))
-            break
+            return 0
+
         crossroad = stack.pop()
         curr_depth = stack_depths.pop()
-        if curr_depth > depth:
-            continue
         visited.append(crossroad)
         visited_depths.append(curr_depth)
+        update_path(curr_depth, crossroad)
+
+        for i in range(len(crossroad)):     # prvé (červené) auto sa nachádza na konci križovatky
+            if crossroad[i][5] == 'A':
+                print("{} depth. Finito.".format(depth))
+                return 1
+
+        if curr_depth > depth:
+            continue
+
         for car_name in cars.keys():
             if cars[car_name]["direction"] == "h":
-                end = move_combo_bez_objektov(crossroad, car_name, visited, visited_depths, stack, stack_depths, curr_depth,
-                                              depth, "right")
-                if end == 1:
-                    print_crossroad_bez_objektov(stack.pop())
-                    return 1
-                move_combo_bez_objektov(crossroad, car_name, visited, visited_depths, stack, stack_depths, curr_depth,
-                                        depth, "left")
+                move_combo_bez_objektov(crossroad, car_name, curr_depth, depth, "right")
+                move_combo_bez_objektov(crossroad, car_name, curr_depth, depth, "left")
             elif cars[car_name]["direction"] == "v":
-                move_combo_bez_objektov(crossroad, car_name, visited, visited_depths, stack, stack_depths, curr_depth,
-                                        depth, "down")
-                move_combo_bez_objektov(crossroad, car_name, visited, visited_depths, stack, stack_depths, curr_depth,
-                                        depth, "up")
-
-    return 0
+                move_combo_bez_objektov(crossroad, car_name, curr_depth, depth, "down")
+                move_combo_bez_objektov(crossroad, car_name, curr_depth, depth, "up")
 
 
 def iterative_dfs_bez_objektov():
@@ -178,19 +287,31 @@ def iterative_dfs_bez_objektov():
         crossroad = init_bez_objektov()
         end = dfs_bez_objektov(crossroad, depth)
         if end == 1:
-            return
-    print("riesenie sa nenaslo")
+            return 1
+    return 0
 
 
 def start_bez_objektov():
-    start_time = time.time()
     print("""-------------------------------------------------------------
     Dobrý večer, dobrý večer. Hráte hru Multikrižovatkár.
    -------------------------------------------------------
     """)
-    iterative_dfs_bez_objektov()
+    start_time = time.time()
+    done = iterative_dfs_bez_objektov()
     end_time = time.time()
-    print("Compilation time: {} seconds".format(round(end_time - start_time, 2)))
+    if done == 1:
+        counter = 0
+        for crossroad in path:
+            if 0 < counter < len(path):
+                print("\n{}. move".format(counter))
+                print_operators(path[counter - 1], path[counter])
+            else:
+                print("\nStarting crossroad:")
+            print_crossroad(crossroad)
+            counter += 1
+    else:
+        print("riesenie sa nenaslo")
+    print("\nCompilation time: {} seconds".format(round(end_time - start_time, 2)))
 
 
 start_bez_objektov()
